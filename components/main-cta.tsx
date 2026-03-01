e8e5e104b9069cb80454d4ad76e6942fd831f789
Jayadev <jayadevnair05@gmail.com>
2026-03-02 02:03:44 +0530

    bug fix

--- components/main-cta.tsx
+++ components/main-cta.tsx
@@ -1,8 +1,10 @@
 "use client"
 
 import { motion } from "framer-motion"
+
+
 import { Button } from "./ui/button"
-import { Link } from "lucide-react"
+import Link from "next/link"
 
 
 export function CTASection() {
